4e75ee508125ae21284662183b37778bde88710c
JasperKS <kjdlsarmiento@gmail.com>
2025-01-14 05:31:33 -0500

    updated dist

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
-
-app.use(cors())
-app.use(express.static('dist'))
 
 let notes = [
     {
@@ -23,6 +19,8 @@ let notes = [
     }
 ]
 
+app.use(express.static('dist'))
+
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -31,6 +29,10 @@ const requestLogger = (request, response, next) => {
   console.log('---')
   next()
 }
+
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json())
 app.use(requestLogger)
