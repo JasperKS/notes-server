3cecfc03481562fd83f5cee2547cd2bfd4598467
JasperKS <kjdlsarmiento@gmail.com>
2025-03-25 05:57:14 -0400

    fixed middleware and added testing module

--- utils/middleware.js
+++ utils/middleware.js
@@ -2,8 +2,8 @@ const logger = require('./logger')
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
-  logger.info('Path:', request.Path)
-  logger.info('Body:', request.Body)
+  logger.info('Path:', request.path)
+  logger.info('Body:', request.body)
   logger.info('---')
   next()
 }
